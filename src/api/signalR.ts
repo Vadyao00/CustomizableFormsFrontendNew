969829724e81fc57899bc4e87281166324bcf907
Vadyao00 <vadim.ivashko.04@gmail.com>
2025-04-21 21:20:37 +0300

    changed remote url

--- src/api/signalR.ts
+++ src/api/signalR.ts
@@ -1,7 +1,7 @@
 import * as signalR from '@microsoft/signalr';
 import { Comment } from '../types';
 
-const API_URL = 'https://customizableforms.runasp.net';
+const API_URL = 'https://customizable-forms-backend.vercel.app';
 
 class SignalRConnection {
   private static instance: SignalRConnection;
